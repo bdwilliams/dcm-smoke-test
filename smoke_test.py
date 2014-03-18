1c7b627c2889af92a4f953639782339317308222
Brian Williams <brian@mxapps.com>
2014-03-18 13:57:04 -0500

    Ability to launch in specified CM environment.

--- smoke_test.py
+++ smoke_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
- 
+
 import os, string, random, argparse, sys, time
 from prettytable import PrettyTable
 from datetime import datetime
@@ -27,8 +27,6 @@ server_launch_avg = []
 snapshots_created = []
 images_created = []
 averages = [int(time.time())]
-cm_account_id = None
-cm_scripts = None
 
 # TODO:
 # Cleanup ROOT-mounted Volumes.
@@ -41,17 +39,17 @@ def watch_jobs():
 	job_averages = []
 	print("Processing jobs... Please hold.")
 	job_table = PrettyTable(["ID", "Status", "Description", "Minutes to Complete"])
-	
+
 	while len(jobs) > 0:
 		for i in jobs:
 			if Job.wait_for(i) == True:
-				the_job = Job(i)           
+				the_job = Job(i)
 				start = datetime.strptime(the_job.start_date.split("+")[0], '%Y-%m-%dT%H:%M:%S.%f')
 				end = datetime.strptime(the_job.end_date.split("+")[0], '%Y-%m-%dT%H:%M:%S.%f')
 				min_to_comp = (end - start).seconds
 				job_averages.append(min_to_comp)
 				job_table.add_row([the_job.job_id, the_job.status, the_job.description, round((min_to_comp/60),3)])
-				
+
 				if 'Launch Server' in the_job.description:
 					servers_launched.append(int(the_job.message))
 				elif 'CREATE VOLUME' in the_job.description:
@@ -67,7 +65,7 @@ def watch_jobs():
 	average = round((sum(job_averages)/60)/len(job_averages),3)
 	averages.append(average)
 	job_table.add_row(["--","--","--",average])
-	
+
 	print job_table
 
 if __name__ == '__main__':
@@ -88,7 +86,7 @@ if __name__ == '__main__':
 	parser.add_argument('--nosnapshots', '-ns', help='Skip Volume Snapshotting', action='store_true')
 	parser.add_argument('--novolumes', '-nv', help='Skip Volume Creation/Attaching', action='store_true')
 	cmd_args = parser.parse_args()
-	
+
 	if cmd_args.account is not None and cmd_args.region is not None and cmd_args.servers is not None and cmd_args.product is not None and cmd_args.datacenter is not None and cmd_args.machineimage is not None and cmd_args.budget is not None:
 		account_id = cmd_args.account
 		region_id = int(cmd_args.region)
@@ -105,72 +103,72 @@ if __name__ == '__main__':
 	else:
 		account_table = PrettyTable(["Account ID", "Account Name"]);
 		start = time.time()
-		
+
 		for account in Account.all():
 			if account.status == 'ACTIVE':
 				account_table.add_row([account.account_id, account.name])
-		
+
 		print 'Results returned in', time.time()-start, 'seconds.'
 		print account_table
-		
+
 		account_id = input("Enter an Account ID: ")
-		
+
 		region_table = PrettyTable(["Region ID", "Region Name"]);
 		start = time.time()
-		
+
 		params = {'account_id':account_id}
 		for region in Region(params).all():
 			region_table.add_row([region.region_id, region.name])
-		
+
 		print 'Results returned in', time.time()-start, 'seconds.'
 		print region_table
-		
+
 		region_id = input("Enter a Region ID: ")
-		
+
 		datacenter_table = PrettyTable(["Datacenter ID", "Datacenter Name"])
 		start = time.time()
-		
+
 		for dc in DataCenter.all(region_id):
 			datacenter_table.add_row([dc.data_center_id, dc.description])
-		
+
 		print 'Results returned in', time.time()-start, 'seconds.'
 		print datacenter_table
-		
+
 		data_center_id = input("Enter a Datacenter ID: ")
 
 		machine_image_table = PrettyTable(["Machine Image ID", "Machine Image Name", "Provider ID"])
 		start = time.time()
-		
+
 		for mi in MachineImage.all(region_id):
 			machine_image_table.add_row([mi.machine_image_id, mi.description, mi.provider_id])
-		
+
 		print 'Results returned in', time.time()-start, 'seconds.'
 		print machine_image_table
-		
+
 		machine_image_id = input("Enter a Machine Image ID: ")
-		
+
 		billing_code_table = PrettyTable(["Budget ID", "Budget Name", "Budget Code", "Status"])
 		start = time.time()
-		
+
 		for budget in BillingCode.all():
 			billing_code_table.add_row([budget.billing_code_id, budget.name, budget.finance_code, budget.budget_state])
-		
+
 		print 'Results returned in', time.time()-start, 'seconds.'
 		print billing_code_table
-		
+
 		billing_code_id = input("Enter a Billing Code ID: ")
-		
+
 		server_product_table = PrettyTable(["Provider Product ID", "Name", "Platform", "Currency", "Hourly Rate"])
 		start = time.time()
-		
+
 		for sp in ServerProduct.all(region_id):
 			server_product_table.add_row([sp.provider_product_id, sp.name, sp.platform, sp.currency, sp.hourly_rate])
-		
+
 		print 'Results returned in', time.time()-start, 'seconds.'
 		print server_product_table
-		
+
 		server_product_id = raw_input("Enter a Server Product: ")
-		
+
 		yes = set(['yes','y', 'ye', ''])
 		no = set(['no','n'])
 
@@ -182,11 +180,11 @@ if __name__ == '__main__':
 				start = time.time()
 				for n in Network.all(region_id=region_id):
 					network_table.add_row([n.network_id, n.name, n.network_address])
-			
+
 				print network_table
-	
+
 				network_id = raw_input("Enter a Network (Hit Enter for None): ")
-				
+
 				if network_id is None:
 					network_id = 0
 			else:
@@ -199,26 +197,26 @@ if __name__ == '__main__':
 		if do_cm is not None and do_cm in yes:
 			cm_account = PrettyTable(["Status", "CM ID#", "Name", "Endpoint"]);
 			start = time.time()
-			
+
 			if len(ConfigurationManagementAccount.all()) > 0:
 				for cm in ConfigurationManagementAccount.all():
 					if cm.status == 'ACTIVE':
 						cm_account.add_row([cm.status, cm.cm_account_id, cm.cm_service['name'], cm.cm_service['service_endpoint']])
-				
+
 				print 'Results returned in', time.time()-start, 'seconds.'
 				print cm_account
-				
+
 				cm_account_id = input("Enter a Configuration Management Account ID (Hit Enter for None): ")
-				
+
 				if cm_account_id is not None:
 					environments = PrettyTable(["ID", "Status", "Description"])
 					start = time.time()
 					for env in Environment.all(cm_account_id):
 						if env['status'] == 'ACTIVE':
-							environments.add_row([env['environmentId'], env['status'], env['description'])
+							environments.add_row([env['sharedEnvironmentCode'], env['status'], env['description']])
 
 					print 'Results returned in', time.time()-start, 'seconds.'
-					print personalities
+					print environments
 
 					cm_environment = raw_input("Enter Environment ID: ")
 
@@ -227,7 +225,7 @@ if __name__ == '__main__':
 					for p in Personality().all(cm_account_id):
 						if p['status'] == 'ACTIVE':
 							personalities.add_row([p['sharedPersonalityCode'], p['name']])
-					
+
 					print 'Results returned in', time.time()-start, 'seconds.'
 					print personalities
 
@@ -238,10 +236,10 @@ if __name__ == '__main__':
 					for sc in Script().all(cm_account_id):
 						if sc['status'] == 'ACTIVE':
 							scripts.add_row([sc['sharedScriptCode'], sc['name']])
-					
+
 					print 'Results returned in', time.time()-start, 'seconds.'
 					print scripts
-					
+
 					cm_scripts = raw_input("Enter Script IDs (comma delimited - no spaces; enter for none): ") or None
 		else:
 			cm_account_id = None
@@ -249,20 +247,20 @@ if __name__ == '__main__':
 			cm_scripts = None
 
 		skip_volumes = raw_input("Would you like to skip volume creation and attachment? (type yes or no): ")
-		
+
 		if skip_volumes in yes:
 			cmd_args.novolumes = True
 
 		skip_snapshots = raw_input("Would you like to skip snapshot creation? (type yes or no): ")
-		
+
 		if skip_snapshots in yes:
 			cmd_args.nosnapshots = True
 
 		skip_imaging = raw_input("Would you like to skip machine imaging? (type yes or no): ")
-		
+
 		if skip_imaging in yes:
 			cmd_args.noimaging = True
-				
+
 		total_servers = input("How many resources would you like to create at a time (ie: 3)? ")
 
 print "###"
@@ -279,7 +277,7 @@ if cm_account_id is not None:
 
 if p_scripts is not None:
 	print "# CM Personalities:\t\t%s" % p_scripts
-	
+
 if cm_scripts is not None:
 	print "# CM Scripts:\t\t%s" % cm_scripts
 
@@ -309,7 +307,7 @@ if cm_scripts is not None:
 
 if p_scripts is not None:
 	run += ' -cp '+str(p_scripts)
-	
+
 if cmd_args.novolumes:
 	run += ' -nv'
 
@@ -322,8 +320,6 @@ if cmd_args.noimaging:
 run += ' -s '+str(total_servers)
 
 print run
-
-sys.exit(1)
 
 print "###"
 print "# Subscriptions:"
@@ -375,19 +371,20 @@ if sub[0]['subscribedServer']:
 
 		if network_id is not None and network_id != '0':
 			new_server.vlan = int(network_id)
-		
+
 		if cm_account_id is not None:
 			new_server.cmAccount = int(cm_account_id)
+			new_server.environment = cm_environment
 
 			if p_scripts is not None:
-				new_server.p_scripts = p_scripts	
-						
+				new_server.p_scripts = p_scripts
+
 			if cm_scripts is not None:
 				new_server.cm_scripts = cm_scripts
 
 		job_id = new_server.launch()
 		jobs.append(job_id)
-	
+
 	# Watch server launch jobs
 	watch_jobs()
 
@@ -403,7 +400,7 @@ if sub[0]['subscribedVolume'] and cmd_args.novolumes is False:
 		result = new_volume.create()
 		print("Creating Volume : %s" % name)
 		jobs.append(result.job_id)
-	
+
 	# Watch volume create jobs
 	watch_jobs()
 
@@ -414,7 +411,7 @@ if sub[0]['subscribedVolume'] and cmd_args.novolumes is False:
 			result = Volume(volumes_created[si]).attach(av)
 			jobs.append(result.current_job)
 			si += 1
-		
+
 		# Watch volume attach jobs
 		watch_jobs()
 
@@ -427,7 +424,7 @@ if sub[0]['subscribedSnapshot'] and cmd_args.nosnapshots is False and cmd_args.n
 		result = volume.snapshot()
 		print "Snapshoting volume : #%s" % (sc)
 		jobs.append(result.current_job)
-	
+
 	# Watch snapshot jobs
 	watch_jobs()
 
@@ -442,7 +439,7 @@ if sub[0]['subscribedMachineImage'] and sub[0]['subscribedServer'] and cmd_args.
 		print "Imaging server : #%s" % (mi)
 		jobs.append(results)
 		sv += 1
-		
+
 	# Watch imaging jobs
 	watch_jobs()
 
